136d4694bd9f9a9c33f01a8c227a4ee0fd749ea2
Sanidhya Sharma <75783551+Sanidhya0504@users.noreply.github.com>
2021-04-02 20:48:16 +0530

    Update ocrbot.py

--- ocrbot.py
+++ ocrbot.py
@@ -47,7 +47,7 @@ def convert_image(update, context):
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    key = os.environ['token']
+    key = os.environ.get("token","")
     updater = Updater(key, use_context=True)
 
     # Get the dispatcher to register handlers
